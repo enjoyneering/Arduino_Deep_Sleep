0c456149e054b49c4d03c15f5dc9987da71b5542
enjoyneering <enjoyneering79@gmail.com>
2016-06-06 17:29:55 -0400

    Update arduino_deep_sleep.ino

--- arduino_deep_sleep.ino
+++ arduino_deep_sleep.ino
@@ -1,40 +1,57 @@
-/*****************************************************************************/
+/************************************************************************************************************/
 /*
-  Arduino_deep_sleep is an example of the proper way to put ANY arduino to sleep
+  "Arduino_deep_sleep" is an example of the proper and most advanced way to
+  put ANY Arduino boards to sleep.
   
-  Puts arduino in to deep sleep for 30 sec. Wakes it up and turns on LED
-  connected to the pin 0. After put arduino back to sleep
+  Puts any arduino in to deep sleep for 30 sec. Wakes it up and turns on LED
+  connected to the pin 0 and puts arduino back to sleep.
+  
+  Average ATtiny85 consumption in deep sleep mode is about 3 microamps
+  at 3.3 volt.
 
   written by enjoyneering79
-
 */
-/*****************************************************************************/
-#include <avr/sleep.h>     //AVR MCU power management
-#include <avr/power.h>     //disable/anable AVR MCU peripheries (Analog Comparator, ADC, USI, Timers/Counters)
-#include <avr/wdt.h>       //AVR MCU watchdog timer
-#include <avr/io.h>        //includes the apropriate AVR MCU IO definitions
-#include <avr/interrupt.h> //manipulation of the interrupt flags
+/************************************************************************************************************/
+#include <avr/sleep.h>     //AVR MCU power management.
+#include <avr/power.h>     //AVR MCU peripheries (Analog comparator, ADC, USI, Timers/Counters etc) management.
+#include <avr/wdt.h>       //AVR MCU watchdog timer management.
+#include <avr/io.h>        //AVR MCU IO ports management.
+#include <avr/interrupt.h> //AVR MCU interrupt flags management.
 
-/**************************************************************************/
+
+/************************************************************************************************************/
 /*
-    variables
+    Constants
+
+    Enables interrupts (instead of MCU reset), when watchdog is timed out.
+    Used for wake-up MCU from power-down/sleep.
 */
-/**************************************************************************/
-byte pinLed = 0;
+/************************************************************************************************************/
+#define WDTCR |= _BV(WDIE)
+
+
+/************************************************************************************************************/
+/*
+    Variables
+*/
+/************************************************************************************************************/
+byte led_pin = 0;
 volatile byte watchdog_counter;
 
-/**************************************************************************/
-/*
-    set all pins as output
 
-    to prevent MCU wake up from a random noise signal during a sleep
+/************************************************************************************************************/
+/*
+    all_pins_output()
     
-    NOTE: you can leave the pins as outputs during a sleep if you connect 
+    Sets all pins as output to prevent MCU wake up from a random noise signal
+    during a sleep. This function is optional.
+    
+    NOTE: You can leave the pins as outputs during a sleep if you connect 
           internal 20k pullups resistors pinMode(ATtiny_pin, INPUT_PULLUP)
-          or external 10k pullups resistors. but it increase the consumption
-          by 1.25uA
+          or external 10k pullups resistors. However it will increase the
+          consumption by 1.25 microamps.
 */
-/**************************************************************************/
+/************************************************************************************************************/
 void all_pins_output()
 {
   for (byte ATtiny_pin; ATtiny_pin < 5; ATtiny_pin++)
@@ -43,11 +60,14 @@ void all_pins_output()
   }
 }
 
-/**************************************************************************/
+
+/************************************************************************************************************/
 /*
-    set back all pins as input
+    all_pins_input()
+    
+    Sets all ATtiny pins as input. This function is optional.
 */
-/**************************************************************************/
+/************************************************************************************************************/
 void all_pins_input()
 {
   for (byte ATtiny_pin; ATtiny_pin < 5; ATtiny_pin++)
@@ -56,22 +76,28 @@ void all_pins_input()
   }
 }
 
-/**************************************************************************/
+
+/************************************************************************************************************/
 /*
-    setup()
+    Setup()
+
+    Main setup
 */
-/**************************************************************************/
+/************************************************************************************************************/
 void setup()
 {
-  pinMode(pinLed,OUTPUT);
+  pinMode(led_pin,OUTPUT);
   setup_watchdog(WDTO_8S); //approximately 8 sec. of sleep
 }
 
-/**************************************************************************/
+
+/************************************************************************************************************/
 /*
-    main loop()
+    loop()
+
+    Main loop
 */
-/**************************************************************************/
+/************************************************************************************************************/
 void loop()
 {
   while (watchdog_counter < 4) //wait for watchdog counter reched the limit (WDTO_8S * 4 = 32sec.)
@@ -87,30 +113,37 @@ void loop()
   power_timer1_enable();    //enable Timer1
   power_usi_enable();       //enable the Universal Serial Interface module
   */
-  delay(5); //to settle down ADC and peripheries
+  delay(5);                 //to settle down the ADC and peripheries
 
-  all_pins_output();
-  digitalWrite(pinLed,HIGH);  //led blink
+  //all_pins_output();
+  digitalWrite(led_pin,HIGH);  //led blink
   delay(3000);
-  digitalWrite(pinLed,LOW);
-
-  all_pins_output();
+  digitalWrite(led_pin,LOW);
+  //all_pins_output();
 }
 
-/**************************************************************************/
+
+/************************************************************************************************************/
 /*
     ATtiny85_sleep()
 
-    put system into the sleep state
-    There are 6 different sleeps modes:
-     *  SLEEP_MODE_IDLE        - the least power savings (CPU stopped but Analog Comparator, ADC, USI, Timer/Counter, Watchdog (if enabled), and the interrupt system to continue operating) (default)
-     *  SLEEP_MODE_ADC         - ADC Noise Reduction (CPU stopped but the ADC, the external interrupts, and the Watchdog (if enabled) to continue operating)
-     *  SLEEP_MODE_PWR_SAVE    - supported by atiny25, atiny45, atiny85
-     *  SLEEP_MODE_EXT_STANDBY - not supported by atiny25, atiny45, atiny85
-     *  SLEEP_MODE_STANDBY     - not supported by atiny25, atiny45, atiny85
-     *  SLEEP_MODE_PWR_DOWN    - the most power savings (all oscillators are stopped, only an External Reset, Watchdog Reset, Brown-out Reset, USI start condition interupt, an external level interrupt on INT0 or a pin change interrupt can wake up the MCU.      
+    Puts MCU into the sleep state
+
+    NOTE: There are 6 different sleeps modes:
+          * SLEEP_MODE_IDLE..........The least power savings state. CPU stopped but Analog
+                                     ccomparator, ADC, USI, Timer/Counter, Watchdog (if enabled),
+                                     & the interrupt system continues operating. (by default in "sleep.h")
+          * SLEEP_MODE_ADC...........ADC Noise Reduction. CPU stopped but the ADC, the external
+                                     interrupts, & the Watchdog (if enabled) continue operating.
+          * SLEEP_MODE_PWR_SAVE......Supported by Atiny25, Atiny45, Atiny85.
+          * SLEEP_MODE_EXT_STANDBY...Not supported by Atiny25, Atiny45, Atiny85.
+          * SLEEP_MODE_STANDBY.......Not supported by Atiny25, Atiny45, Atiny85.
+          * SLEEP_MODE_PWR_DOWN......The most power savings state. All oscillators are stopped, only an
+                                     External Reset, Watchdog Reset, Brown-out Reset, USI start condition
+                                     interupt & external level interrupt on INT0 or a pin change interrupt
+                                     can wake up the MCU.      
 */
-/**************************************************************************/
+/************************************************************************************************************/
 void ATtiny85_sleep()
 {
   power_all_disable();                 //disable all peripheries (timer0, timer1, Universal Serial Interface, ADC)
@@ -125,32 +158,34 @@ void ATtiny85_sleep()
                                          (so the sleep is possible), goes to sleep, wakes-up from sleep after an
                                          interrupt (if interrupts are enabled) or WDT timed out (if enabled) and
                                          clears the SE (Sleep Enable) bit afterwards).
-                                         the sketch will continue from this point after interrupt or WDT timed out*/
+                                         the sketch will continue from this point after interrupt or WDT timed out
+                                       */
 }
 
-/**************************************************************************/
+
+/************************************************************************************************************/
 /*
     setup_watchdog()
     
     Sleeps intervals: WDTO_15MS, WDTO_30MS, WDTO_60MS, WDTO_120MS, WDTO_250MS,
                       WDTO_500MS, WDTO_1S, WDTO_2S, WDTO_4S, WDTO_8S
     
-    NOTE: MCU runs from internal 128kHz clock and continues to work during the
-          deepest sleep modes to provide a wake up source.
+    NOTE: The MCU watchdog runs from internal 128kHz clock and continues to work
+          during the deepest sleep modes to provide a wake up source.
 */
-/**************************************************************************/
+/************************************************************************************************************/
 void setup_watchdog(byte sleep_time)
 {
   wdt_enable(sleep_time);
-  WDTCR |= _BV(WDIE);     /*enable interrupts instead of MCU reset when watchdog is timed out
-                            used for wake-up MCU from power-down*/
 }
 
-/**************************************************************************/
+/************************************************************************************************************/
 /*
-    Watchdog Interrupt Service (executed when watchdog is timed out)
+    ISR(WDT_vect)
+    
+    Watchdog Interrupt Service (automatically executed when watchdog is timed out)
 */
-/**************************************************************************/
+/************************************************************************************************************/
 ISR(WDT_vect)
 {
   watchdog_counter++;
