b2ddd6a88bd1f5f0919838912a50ce93eb7fe0ee
enjoyneering <enjoyneering79@gmail.com>
2018-02-20 16:12:04 -0500

    Update arduino_deep_sleep.ino

--- arduino_deep_sleep.ino
+++ arduino_deep_sleep.ino
@@ -1,17 +1,18 @@
-/*****************************************************************************/
+/***************************************************************************************************/
 /*
-  Arduino_deep_sleep is an example of the proper way to put any AVR based
-  Arduino in to sleep
+  Arduino_deep_sleep is an example of the proper and most advanced way to put any AVR based Arduino
+  boards in to sleep
   
   Sketch puts Arduino in to the deep sleep for ~30 sec. Wakes it up, turns on/off
-  the LED connected to the pin0 & puts Arduino back to sleep.
+  the LED connected to the pin0 or pin13 & puts Arduino back to sleep.
 
   written by : enjoyneering79
   sourse code: https://github.com/enjoyneering/Arduino_Deep_Sleep
 
-  BSD license, all text above must be included in any redistribution
+  GNU GPL license, all text above must be included in any redistribution, see link below for details:
+  - https://www.gnu.org/licenses/licenses.html
 */
-/*****************************************************************************/
+/***************************************************************************************************/
 #include <avr/sleep.h>     //AVR MCU power management
 #include <avr/power.h>     //disable/anable AVR MCU peripheries (Analog Comparator, ADC, USI, Timers/Counters)
 #include <avr/wdt.h>       //AVR MCU watchdog timer
@@ -23,19 +24,24 @@
     variables
 */
 /**************************************************************************/
+#if defineddefined(__AVR_ATmega328P__) || defined(__AVR_ATmega168p__)
+byte led = LED_BUILTIN;
+#elif (__AVR_ATtiny85__) || defined(__AVR_ATtiny45__) || defined(__AVR_ATtiny25__)
+byte led = 0;
+#endif
+
 volatile byte watchdogCounter = 0;
-         byte led             = 0;
 
 /**************************************************************************/
 /*
     all_pins_output()
     
-    Sets all pins as output, to prevent MCU wake up from a random noise
-    signal during a sleep. Use it if needed.
+    Sets all pins as output, to prevent MCU wake up from random noise
+    signal during sleep, use if needed
     
     NOTE:
-    - pinMode(ATtiny_pin, INPUT_PULLUP) connects internal 20k pullups resistors
-    - it increases the consumption by 1.25uA
+    - INPUT_PULLUP connects internal 20k..30k pullups resistors & increase
+      consumption by 1.25uA
 */
 /**************************************************************************/
 void all_pins_output()
@@ -66,6 +72,19 @@ void all_pins_input()
     setup()
 
     Main setup
+
+    NOTE:
+    - valid sleeps intervals:
+      * WDTO_15MS
+      * WDTO_30MS
+      * WDTO_60MS
+      * WDTO_120MS
+      * WDTO_250MS
+      * WDTO_500MS
+      * WDTO_1S
+      * WDTO_2S
+      * WDTO_4S
+      * WDTO_8S
 */
 /**************************************************************************/
 void setup()
@@ -89,7 +108,9 @@ void loop()
     arduino_sleep();
   }
   
-  watchdogCounter = 0;        //reset watchdogCounter
+  //wdt_disable();            //disable & stop wdt timer
+  watchdogCounter = 0;        //reset counter
+
   power_all_enable();         //enable all peripheries (timer0, timer1, Universal Serial Interface, ADC)
   /*
   power_adc_enable();         //enable ADC
@@ -97,34 +118,39 @@ void loop()
   power_timer1_enable();      //enable Timer1
   power_usi_enable();         //enable the Universal Serial Interface module
   */
-  delay(5);                   //to settle down ADC and peripheries
+  delay(5);                   //to settle down ADC & peripheries
 
-  digitalWrite(led,HIGH);     //led blink
+  digitalWrite(led, HIGH);    //led blink
   delay(3000);
-  digitalWrite(led,LOW);
+  digitalWrite(led, LOW);
+
+  //wdt_enable(WDTO_8S);      //enable wdt timer
 }
 
 /**************************************************************************/
 /*
     arduino_sleep()
 
-    Puts system into the sleep state.
-    
-     There are 6 different sleeps modes:
-     *  SLEEP_MODE_IDLE        - the least power savings (CPU stopped but Analog Comparator, ADC, USI, Timer/Counter,
-                                 Watchdog (if enabled), and the interrupt system to continue operating) (default)
-     *  SLEEP_MODE_ADC         - ADC Noise Reduction (CPU stopped but the ADC, the external interrupts,
-                                 and the Watchdog (if enabled) to continue operating)
-     *  SLEEP_MODE_PWR_SAVE    - supported by atiny25, atiny45, atiny85
-     *  SLEEP_MODE_EXT_STANDBY - not supported by atiny25, atiny45, atiny85
-     *  SLEEP_MODE_STANDBY     - not supported by atiny25, atiny45, atiny85
-     *  SLEEP_MODE_PWR_DOWN    - the most power savings: all oscillators are stopped, only an External Reset, Watchdog Reset,
-                                 Brown-out Reset, USI start condition interupt, an external level interrupt on INT0 or a pin
-                                 change interrupt can wake up the MCU.      
+    Puts system into the sleep state
+ 
+    NOTE:   
+    - there are 6 different sleeps modes:
+      *  SLEEP_MODE_IDLE        - the least power savings (CPU stopped but Analog Comparator, ADC, USI, Timer/Counter,
+                                  Watchdog (if enabled), and the interrupt system to continue operating) (default)
+      *  SLEEP_MODE_ADC         - ADC Noise Reduction (CPU stopped but the ADC, the external interrupts,
+                                  and the Watchdog (if enabled) to continue operating)
+      *  SLEEP_MODE_PWR_SAVE    - supported by atiny25, atiny45, atiny85
+      *  SLEEP_MODE_EXT_STANDBY - not supported by atiny25, atiny45, atiny85
+      *  SLEEP_MODE_STANDBY     - not supported by atiny25, atiny45, atiny85
+      *  SLEEP_MODE_PWR_DOWN    - the most power savings: all oscillators are stopped, only an External Reset, Watchdog Reset,
+                                  Brown-out Reset, USI start condition interupt, an external level interrupt on INT0 or a pin
+                                  change interrupt can wake up the MCU.      
 */
 /**************************************************************************/
 void arduino_sleep()
 {
+  cli();                               //disable interrupts for time critical operations below
+
   power_all_disable();                 //disable all peripheries (timer0, timer1, Universal Serial Interface, ADC)
   /*              
   power_adc_disable();                 //disable ADC
@@ -134,11 +160,14 @@ void arduino_sleep()
   */
   set_sleep_mode(SLEEP_MODE_PWR_DOWN); //set the sleep type
 
-  #if defined(__AVR_ATmega328P__) || (__AVR_ATtiny85__) || defined (__AVR_ATtiny45__) || defined (__AVR_ATtiny25__)
-  sleep_bod_disable();                 //disable brown out detection during the sleep. saves more power
+  #if defined(__AVR_ATmega328P__) || (__AVR_ATtiny85__) || defined(__AVR_ATtiny45__) || defined(__AVR_ATtiny25__)
+  sleep_bod_disable();                 //disable brown out detection during the sleep to saves more power
   #endif
 
-  sleep_mode();                        /*system stops & sleeps here (automatically sets the SE (Sleep Enable) bit
+  sei();                               //re-enable interrupts
+
+  sleep_mode();                        /*
+                                         system stops & sleeps here (automatically sets the SE (Sleep Enable) bit
                                          (so the sleep is possible), goes to sleep, wakes-up from sleep after the
                                          interrupt (if interrupts are enabled) or WDT timed out (if enabled) and
                                          clears the SE (Sleep Enable) bit afterwards).
@@ -152,32 +181,46 @@ void arduino_sleep()
 
     Sets up watchdog to trigger interrupt after watchdog is timed out
     
-    Sleeps intervals:
-    - WDTO_15MS, WDTO_30MS, WDTO_60MS, WDTO_120MS, WDTO_250MS,
-      WDTO_500MS, WDTO_1S, WDTO_2S, WDTO_4S, WDTO_8S
-    
     NOTE:
-    - MCU runs from internal 128kHz clock and continues to work during the
-      deepest sleep modes to provide a wake up source.
+    - WDT runs from internal 128kHz clock & continues to work during the
+      deepest sleep modes to provide a wake up source
+    - valid sleeps intervals:
+      * WDTO_15MS
+      * WDTO_30MS
+      * WDTO_60MS
+      * WDTO_120MS
+      * WDTO_250MS
+      * WDTO_500MS
+      * WDTO_1S
+      * WDTO_2S
+      * WDTO_4S
+      * WDTO_8S
 */
 /**************************************************************************/
 void setup_watchdog(byte sleep_time)
 {
-  /* enables interrupts instead of MCU reset, when watchdog is timed out */
-  #if   defined(__AVR_ATmega328P__) || defined (__AVR_ATmega168p__)
+  cli();                  //disable interrupts for time critical operations below
+
+  /* enable only watchdog interrupts */
+  #if   defined(__AVR_ATmega328P__) || defined(__AVR_ATmega168p__)
   WDTCSR |= _BV(WDIE);
-  #elif defined (__AVR_ATtiny85__) || defined (__AVR_ATtiny45__) || defined (__AVR_ATtiny25__)
+  #elif defined(__AVR_ATtiny85__) || defined(__AVR_ATtiny45__) || defined(__AVR_ATtiny25__)
   WDTCR  |= _BV(WDIE);
   #endif
-  
-  wdt_enable(sleep_time);
+
+  wdt_enable(sleep_time); //enable wdt timer
+
+  sei();                  //re-enable interrupts
 }
 
 /**************************************************************************/
 /*
     ISR(WDT_vect)
 
-    Watchdog Interrupt Service, executed when watchdog is timed out
+    Watchdog Interrupt Service Routine, executed when watchdog is timed out
+
+    NOTE:
+    - if WDT ISR is not defined, MCU will reset after WDT
 */
 /**************************************************************************/
 ISR(WDT_vect)
