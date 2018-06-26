bb70937d6b8fc112fe06d9154e431865f8d42fa1
enjoyneering <enjoyneering79@gmail.com>
2018-06-26 15:43:44 -0400

    Update arduino_deep_sleep.ino

--- arduino_deep_sleep.ino
+++ arduino_deep_sleep.ino
@@ -1,10 +1,10 @@
 /***************************************************************************************************/
 /*
-  Arduino_deep_sleep is an example of the proper and most advanced way to put any AVR based Arduino
-  boards in to sleep
+  Arduino example of the proper and most advanced way to put any AVR based Arduino
+  boards into sleep
   
-  Sketch puts Arduino in to the deep sleep for ~30 sec. Wakes it up, turns on/off
-  the LED connected to the pin0 or pin13 & puts Arduino back to sleep.
+  Sketch puts Arduino in to the deep sleep for ~30 sec. Wakes it up, turns on/off LED connected
+  to pin0/pin13 & puts Arduino back to sleep.
 
   written by : enjoyneering79
   sourse code: https://github.com/enjoyneering/Arduino_Deep_Sleep
@@ -19,25 +19,25 @@
 #include <avr/io.h>        //includes the apropriate AVR MCU IO definitions
 #include <avr/interrupt.h> //manipulation of the interrupt flags
 
+#if defined(__AVR_ATtiny25__) || defined(__AVR_ATtiny45__) || defined(__AVR_ATtiny85__) || defined(__AVR_ATtiny87__) || defined(__AVR_ATtiny167__)
+#define LED 0
+#else
+#define LED LED_BUILTIN
+#endif
+
 /**************************************************************************/
 /*
     variables
 */
 /**************************************************************************/
-#if defined(__AVR_ATmega328P__) || defined(__AVR_ATmega168p__)
-byte led = LED_BUILTIN;
-#elif defined(__AVR_ATtiny85__) || defined(__AVR_ATtiny45__) || defined(__AVR_ATtiny25__)
-byte led = 0;
-#endif
-
 volatile byte watchdogCounter = 0;
 
 /**************************************************************************/
 /*
     all_pins_output()
     
-    Sets all pins as output, to prevent MCU wake up from random noise
-    signal during sleep, use if needed
+    Sets all pins as output, to prevent the MCU from waking up from a
+    random noise signal during sleep, use if needed
     
     NOTE:
     - INPUT_PULLUP connects internal 20k..30k pullups resistors & increase
@@ -46,9 +46,9 @@ volatile byte watchdogCounter = 0;
 /**************************************************************************/
 void all_pins_output()
 {
-  for (byte ATtiny_pin; ATtiny_pin < 5; ATtiny_pin++)
+  for (byte pin = 0; pin < 5; pin++) //ATtiny85 has 5 pins
   {
-    pinMode(ATtiny_pin, INPUT_PULLUP);
+    pinMode(pin, INPUT_PULLUP);
   }
 }
 
@@ -61,9 +61,9 @@ void all_pins_output()
 /**************************************************************************/
 void all_pins_input()
 {
-  for (byte ATtiny_pin; ATtiny_pin < 5; ATtiny_pin++)
+  for (byte pin = 0; pin < 5; pin++) //ATtiny85 has 5 pins
   {
-    pinMode(ATtiny_pin, INPUT);
+    pinMode(pin, INPUT);
   }
 }
 
@@ -74,22 +74,14 @@ void all_pins_input()
     Main setup
 
     NOTE:
-    - valid sleeps intervals:
-      * WDTO_15MS
-      * WDTO_30MS
-      * WDTO_60MS
-      * WDTO_120MS
-      * WDTO_250MS
-      * WDTO_500MS
-      * WDTO_1S
-      * WDTO_2S
-      * WDTO_4S
-      * WDTO_8S
+    - valid sleep intervals: WDTO_15MS,  WDTO_30MS,  WDTO_60MS, WDTO_120MS
+                             WDTO_250MS, WDTO_500MS, WDTO_1S,   WDTO_2S
+                             WDTO_4S,    WDTO_8S
 */
 /**************************************************************************/
 void setup()
 {
-  pinMode(led, OUTPUT);
+  pinMode(LED, OUTPUT);
   setup_watchdog(WDTO_8S); //approximately 8 sec. of sleep
 }
 
@@ -102,7 +94,7 @@ void setup()
 /**************************************************************************/
 void loop()
 {
-  while (watchdogCounter < 4) //wait for watchdog counter reched the limit (WDTO_8S * 4 = 32sec.)
+  while (watchdogCounter < 4) //wait for watchdog counter reached the limit, WDTO_8S * 4 = 32sec.
   {
     //all_pins_output();
     arduino_sleep();
@@ -111,7 +103,7 @@ void loop()
   //wdt_disable();            //disable & stop wdt timer
   watchdogCounter = 0;        //reset counter
 
-  power_all_enable();         //enable all peripheries (timer0, timer1, Universal Serial Interface, ADC)
+  power_all_enable();         //enable all peripheries (ADC, Timer0, Timer1, Universal Serial Interface)
   /*
   power_adc_enable();         //enable ADC
   power_timer0_enable();      //enable Timer0
@@ -120,9 +112,9 @@ void loop()
   */
   delay(5);                   //to settle down ADC & peripheries
 
-  digitalWrite(led, HIGH);    //led blink
+  digitalWrite(LED, HIGH);    //led blink
   delay(3000);
-  digitalWrite(led, LOW);
+  digitalWrite(LED, LOW);
 
   //wdt_enable(WDTO_8S);      //enable wdt timer
 }
@@ -144,73 +136,132 @@ void loop()
       *  SLEEP_MODE_STANDBY     - not supported by atiny25, atiny45, atiny85
       *  SLEEP_MODE_PWR_DOWN    - the most power savings: all oscillators are stopped, only an External Reset, Watchdog Reset,
                                   Brown-out Reset, USI start condition interupt, an external level interrupt on INT0 or a pin
-                                  change interrupt can wake up the MCU.      
+                                  change interrupt can wake up the MCU.
+    - anothe useful fitures
+      *  sleep_enable()         - enable sleep, set Sleep Enable (SE) bit
+      *  sleep_cpu()            - system stops & sleeps here, but doesn't automatically sets/clears Sleep Enable (SE) bit like sleep_mode()
+      *  sleep_disable()        - disable sleep, clears Sleep Enable (SE) bit
+      
 */
 /**************************************************************************/
 void arduino_sleep()
 {
   cli();                               //disable interrupts for time critical operations below
 
-  power_all_disable();                 //disable all peripheries (timer0, timer1, Universal Serial Interface, ADC)
+  power_all_disable();                 //disable all peripheries (ADC, Timer0, Timer1, Universal Serial Interface)
   /*              
   power_adc_disable();                 //disable ADC
   power_timer0_disable();              //disable Timer0
   power_timer1_disable();              //disable Timer2
   power_usi_disable();                 //disable the Universal Serial Interface module
   */
-  set_sleep_mode(SLEEP_MODE_PWR_DOWN); //set the sleep type
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN); //set sleep type
 
-  #if defined(__AVR_ATmega328P__) || (__AVR_ATtiny85__) || defined(__AVR_ATtiny45__) || defined(__AVR_ATtiny25__)
-  sleep_bod_disable();                 //disable brown out detection during the sleep to saves more power
+  #if defined(BODS) && defined(BODSE)  //if MCU has bulit-in BOD it will be disabled, ATmega328P, ATtiny85, AVR_ATtiny45, ATtiny25  
+  sleep_bod_disable();                 //disable Brown Out Detector (BOD) before going to sleep, saves more power
   #endif
 
   sei();                               //re-enable interrupts
 
   sleep_mode();                        /*
-                                         system stops & sleeps here (automatically sets the SE (Sleep Enable) bit
-                                         (so the sleep is possible), goes to sleep, wakes-up from sleep after the
-                                         interrupt (if interrupts are enabled) or WDT timed out (if enabled) and
-                                         clears the SE (Sleep Enable) bit afterwards).
-                                         NOTE: sketch will continue from this point after interrupt or WDT timed out!!!
+                                         system stops & sleeps here, it automatically sets Sleep Enable (SE) bit, 
+                                         so sleep is possible, goes to sleep, wakes-up from sleep after interrupt,
+                                         if interrupt is enabled or WDT enabled & timed out, than clears the SE bit.
                                        */
+
+  //NOTE: sketch will continue from this point after interrupt or timed out WDT
 }
 
 /**************************************************************************/
 /*
     setup_watchdog()
 
-    Sets up watchdog to trigger interrupt after watchdog is timed out
+    Sets up watchdog to trigger interrupt, not reset, after watchdog is
+    timed out
     
     NOTE:
     - WDT runs from internal 128kHz clock & continues to work during the
       deepest sleep modes to provide a wake up source
-    - valid sleeps intervals:
-      * WDTO_15MS
-      * WDTO_30MS
-      * WDTO_60MS
-      * WDTO_120MS
-      * WDTO_250MS
-      * WDTO_500MS
-      * WDTO_1S
-      * WDTO_2S
-      * WDTO_4S
-      * WDTO_8S
+    - Your can configure watchdog timer through Watchdog Timer Control
+      Register WDTCSR/WDTCR 
+    - Watchdog timer control register has seven configuration bits:
+      7     6     5     4     3    2     1     0
+      WDIF, WDIE, WDP3, WDCE, WDE, WDP2, WDP1, WDP0
+    - Following  prescaler 4 bits determine how long the timer will count
+      for before resetting:
+      WDP  WDP  WDP  WDP  Time-out  Macro
+      0    0    0    0    16ms      WDTO_15MS
+      0    0    0    1    32ms      WDTO_30MS
+      0    0    1    0    64ms      WDTO_60MS
+      0    0    1    1    125ms     WDTO_120MS
+      0    1    0    0    250ms     WDTO_250MS
+      0    1    0    1    500ms     WDTO_500MS
+      0    1    1    0    1000ms    WDTO_1S
+      0    1    1    1    2000ms    WDTO_2S
+      1    0    0    0    4000ms    WDTO_4S
+      1    0    0    1    8000ms    WDTO_8S
+    - WDE bit Watchdog System Reset Enable, it is overridden by WDRF bit
+      in MCUSR/MCU Status Register. This means that WDE is always set
+      when WDRF is set. To clear WDE, WDRF must be cleared first. This
+      feature ensures multiple resets during conditions causing failure
+      & safe start-up after the failure
+    - WDCE bit is Watchdog Change Enable, it is used in timed sequences for
+      changing WDE & prescaler bits. To clear the WDE bit or change 
+      prescaler bits, WDCE must be set. Once written to one, hardware will 
+      clear WDCE after four clock cycles.
+    - WDIE bit is Watchdog Interrupt Enable. When this bit is written to
+      one & I-bit in MCUSR is set, the Watchdog Interrupt is enabled.
+      If WDE is cleared in combination with this setting, the Watchdog Timer
+      is in Interrupt Mode & corresponding interrupt is executed if time-out
+      in the Watchdog Timer occurs.
+      If WDE is set, the Watchdog Timer is in Interrupt & System Reset Mode.
+      The first time-out in the Watchdog Timer will set WDIF.
+      Executing corresponding interrupt vector will clear WDIE & WDIF
+      automatically by hardware, Watchdog goes to System Reset Mode.
+      This is useful for keeping the Watchdog Timer security while using
+      interrupt. To stay in Interrupt & System Reset Mode, WDIE must be set
+      after each interrupt. This shouldn't be done within interrupt
+      service routine itself, as this might compromise the safety-function
+      of Watchdog System Reset mode. If the interrupt is not executed before
+      next time-out, a System Reset will be applied.
+    - Watchdog Timer Configuration:
+      WDTON  WDE  WDIE  Mode Action    Time-out Action
+      1      0    0     Stopped        None
+      1      0    1     Interrupt      Interrupt
+      1      1    0     System Reset   Reset
+      1      1    1     Interrupt &    Interrupt then go to System Reset
+                        System Reset
+      0      x    x     System Reset   Reset
+      WDTON fuse set to "0" means programmed & "1" means unprogrammed
+    - WDIF is Watchdog System Interrupt Flag & automatically flagged high
+      and low by the system if interrupt happend.
+    - MCUSR is MCU Status Register, it provides information on which reset
+      source caused an MCU reset. MCUSR bit 3 is WDRF Watchdog System Reset
+      Flag. his bit is set if a Watchdog System Reset occurs. The bit is
+      reset by a Power-on Reset, or by writing a logic zero to the flag.
+    - Watchdog is a timer, if you donʼt reset it regularly it will time-out.
+      Just call "wdt_reset()" before timer expires, otherwise a watchdog
+      will reset MCU.
+    - Macros _BV(bit)=(1 << (bit)) converts bit number into a byte value.
 */
 /**************************************************************************/
 void setup_watchdog(byte sleep_time)
 {
-  cli();                  //disable interrupts for time critical operations below
+  cli();                           //disable interrupts for time critical operations below
 
-  /* enable only watchdog interrupts */
-  #if   defined(__AVR_ATmega328P__) || defined(__AVR_ATmega168p__)
-  WDTCSR |= _BV(WDIE);
-  #elif defined(__AVR_ATtiny85__) || defined(__AVR_ATtiny45__) || defined(__AVR_ATtiny25__)
-  WDTCR  |= _BV(WDIE);
+  MCUSR &= ~_BV(WDRF);             //must be cleared first, to clear WDE
+
+  wdt_enable(sleep_time);          //set WDCE, WDE change prescaler bits
+
+  #if defined(__AVR_ATtiny25__) || defined(__AVR_ATtiny45__) || defined(__AVR_ATtiny85__) || defined(__AVR_ATtiny87__) || defined(__AVR_ATtiny167__)
+  WDTCR  |= _BV(WDCE) & ~_BV(WDE); //set WDCE first, clear WDE second, changes have to be done within 4-cycles
+  WDTCR  |= _BV(WDIE);             //set WDIE to Watchdog Interrupt
+  #else
+  WDTCSR |= _BV(WDCE) & ~_BV(WDE); //set WDCE first, clear WDE second, changes have to be done within 4-cycles
+  WDTCSR |= _BV(WDIE);             //set WDIE to Watchdog Interrupt
   #endif
 
-  wdt_enable(sleep_time); //enable wdt timer
-
-  sei();                  //re-enable interrupts
+  sei();                           //re-enable interrupts
 }
 
 /**************************************************************************/
